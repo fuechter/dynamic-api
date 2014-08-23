c1885e71182c70ce7fa51cc30c1415c118cc4c24
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2014-08-23 14:49:30 -0300

    add lib read

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var underscore = require('underscore');
 var defaults = require('defaults');
 
 var lib_ext = require('./lib/ext');
+var lib_read = require('./lib/read');
 
 var pathsImport = [];
 var options = {
@@ -11,34 +12,15 @@ var options = {
     ignoreRepeated: true
 }
 
-var readAlternativePath = function(paths, file) {
-    paths = path.join(paths, file);
-    if (fs.existsSync(paths)) {
-        return paths;
-    } else {
-        for (var i = 0, _len = options.paths.length; i < _len; i++) {
-            var pathAlternative = path.join(options.paths[i], file);
-            if (fs.existsSync(pathAlternative)) {
-                return pathAlternative;
-            }
-        }
-        return paths;
-    }
-};
-
 var getImport = function(ext, contents, dirname) {
     lib_ext.allExt[ext].lastIndex = 0;
     var match = lib_ext[ext].exec(contents);
-    return match ? { matchText: match[0], index: match.index, path: readAlternativePath(dirname, match[(ext === 'html' || ext === 'any' ? 1 : 2 )]), match: match} : undefined;
-};
-
-var readFile = function(_import) {
-    return processFile(_import.path, String(fs.readFileSync(_import.path)));
+    return match ? { matchText: match[0], index: match.index, path: lib_read.readAlternativePath(dirname, match[(ext === 'html' || ext === 'any' ? 1 : 2 )]), match: match} : undefined;
 };
 
 var processMatch = function( _import, contents, hasAlready) {
     return contents.substring(0,_import.index) +
-    (options.ignoreRepeated && hasAlready ? '' : readFile(_import)) +
+    (options.ignoreRepeated && hasAlready ? '' : lib_read.readFile(_import)) +
     contents.substring(_import.index+_import.matchText.length);
 };
 
